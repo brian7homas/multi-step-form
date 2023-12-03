cb14103350f9115142a263adc40f3e72ac76fe24
Brian Thomas <btdeveloper125@gmail.com>
2023-12-03 07:34:04 -0700

    refactor(Menu List): LIST MENU
    iterate through list menu array to render list items

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,37 +1,52 @@
 import * as React from 'react'
 
 import PathP from '../../static/bg-sidebar-desktop.svg'
+
+// List data
+const menuText = [
+  {
+    number: 1,
+    step: 'Step 1',
+    text: 'YOUR INFO'
+  },
+  {
+    number: 2,
+    step: 'Step 2',
+    text: 'SELECT PLAN'
+  },
+  {
+    number: 3,
+    step: 'Step 3',
+    text: 'ADD-ONS'
+  },
+  {
+    number: 4,
+    step: 'Step 4',
+    text: 'SUMMARY'
+  }
+]
 const Menu = () => (
   <div className="menu-outer">
-  <PathP className="menu-bg"/>
+    <PathP className="menu-bg" />
     <div className="menu-inner">
-    
       <div className="menu-container">
         <ul className="menu-list">
-            <li>
-              <small>
-                Step 1
-              </small>
-              YOUR INFO
-            </li>
-            <li>
-              <small>
-                Step 2
-              </small>
-              SELECT PLAN
-            </li>
-            <li>
-              <small>
-                Step 3
-              </small>
-              ADD-ONS
-            </li>
-            <li>
-              <small>
-                Step 4
-              </small>
-              SUMMARY
-            </li>
+          <div className="menu-item-container">
+            {
+              menuText.map(item => {
+                return (
+                  <div className="menu-item-shell">
+                    <li className="menu-item">
+                      <div className="menu-item-style"></div>
+                      <span className="menu-item-style__number">{item.number}</span>
+                      <small>{item.step}</small>
+                      {item.text}
+                    </li>
+                  </div>
+                )
+              })
+            }
+          </div>
         </ul>
       </div>
     </div>
