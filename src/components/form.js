845cf1ddd61c1cc9e65fa47fa8ef9e289e079a48
Brian Thomas <btdeveloper125@gmail.com>
2023-12-03 19:52:22 -0700

    refactor(Form): FORM COMPONENT
    refactored form component to be a function

--- src/components/form.js
+++ src/components/form.js
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React, { useEffect } from "react"
 import gsap from 'gsap'
 import formContainer from '../styles/formContainer.css'
 import menu from '../styles/menu.css'
@@ -10,11 +10,23 @@ import Step1 from "./step1"
 import Step2 from "./step2"
 import Step3 from "./step3"
 import Step4 from "./step4"
-
+function Form() {
+  useEffect(() => {
+  }, [])
+  return (
+    <section className="form-container-outer">
+      <Menu tl={tl} />
+      <div className="form-outer">
+        <div className="form-inner">
           <Step1 />
           <Step2 />
           <Step3 />
           <Step4 />
           <button className="form-submit-reverse">Previous Step</button>
+          <button className="form-submit">Next Step</button>
+        </div>
+      </div>
+    </section>
+  )
 
 export default Form
