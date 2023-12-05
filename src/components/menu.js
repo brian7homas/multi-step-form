2cd9804fb6b0ef8c17f6b663b14f5c8a2ae2436a
Brian Thomas <btdeveloper125@gmail.com>
2023-12-04 19:16:20 -0700

    refactor(Menu): MENU TEXT
    iterate through menu text
    setup component as functional compoent - including return portion

--- src/components/menu.js
+++ src/components/menu.js
@@ -7,50 +7,60 @@ const menuText = [
   {
     number: 1,
     step: 'Step 1',
-    text: 'YOUR INFO'
+    text: 'YOUR INFO',
+    tlName: 'step-1'
   },
   {
     number: 2,
     step: 'Step 2',
-    text: 'SELECT PLAN'
+    text: 'SELECT PLAN',
+    tlName: 'step-2'
   },
   {
     number: 3,
     step: 'Step 3',
-    text: 'ADD-ONS'
+    text: 'ADD-ONS',
+    tlName: 'step-3'
   },
   {
     number: 4,
     step: 'Step 4',
-    text: 'SUMMARY'
+    text: 'SUMMARY',
+    tlName: 'step-4'
   }
 ]
-const Menu = () => (
-  <div className="menu-outer">
-    <PathP className="menu-bg" />
-    <div className="menu-inner">
-      <div className="menu-container">
-        <ul className="menu-list">
-          <div className="menu-item-container">
-            {
-              menuText.map(item => {
-                return (
-                  <div className="menu-item-shell">
-                    <li className="menu-item">
-                      <div className="menu-item-style"></div>
-                      <span className="menu-item-style__number">{item.number}</span>
-                      <small>{item.step}</small>
-                      {item.text}
-                    </li>
-                  </div>
-                )
-              })
-            }
-          </div>
-        </ul>
+  return (
+    <div className="menu-outer">
+      <PathP className="menu-bg" />
+      <div className="menu-inner">
+        <div className="menu-container">
+          <ul className="menu-list">
+            <div className="menu-item-container">
+              {
+                menuText.map((item, index) => {
+                  return (
+                    <div className="menu-item-shell">
+                      <li className="menu-item" onClick={() => {
+                          tl
+                          .timeScale(2)
+                          .tweenTo(`step${index+1}-direct`, {ease: "in.strong"})
+                          setActive(index)
+                        }}>
+                        <div className={`menu-item-style ${item.tlName}`} ></div>
+                        <span className="menu-item-style__number">{item.number}</span>
+                        <small>{item.step}</small>
+                        {item.text}
+                      </li>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          </ul>
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Menu
