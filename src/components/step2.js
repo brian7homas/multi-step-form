4898a89bbf7933fe69b70a2e4a1fab3441faa92a
Brian Thomas <btdeveloper125@gmail.com>
2023-12-08 13:12:23 -0700

    refactor(Step2): OPTIONS
    put options data in an array of objects to iterate over
    added icons to each

--- src/components/step2.js
+++ src/components/step2.js
@@ -1,11 +1,50 @@
 import * as React from 'react'
-
+import Arcade from '../../static/icon-arcade.svg'
+import Advanced from '../../static/icon-advanced.svg'
+import Pro from '../../static/icon-pro.svg'
+import '../styles/step2.css'
+const Cards = [
+  {
+    icon:<Arcade/>,
+    text:'Arcade',
+    price:{
+      monthly: '$9/mo',
+      yearly: '$90/yr'
+    }
+  },
+  {
+    icon:<Advanced/>,
+    text:'Advanced',
+    price:{
+      monthly: '$12/mo',
+      yearly: '$120/yr'
+    }
+  },
+  {
+    icon:<Pro/>,
+    text:'Pro',
+    price:{
+      monthly: '$15/mo',
+      yearly: '$150/yr'
+    }
+  }
+]
 const Step2 = () => (
-  <div className="step2">
-    <h1>Select your plan</h1>
-    <p>Select your plan.</p>
-    
-  </div>
+          {
+            Cards.map((el) =>{
+              return(
+                <div className="step2-tier-option">
+                  <div className="step2-tier-option__inner">
+                    {el.icon}
+                    <h1 className="step2-tier-option__text">{el.text}</h1>
+                    <p className="step2-tier-option__yearly">{el.price.yearly}</p>
+                    {/* <p className="step2-tier-option__monthly">{el.price.monthly}</p> */}
+                    <p className="step2-tier-option__free">2 months free</p>
+                  </div>
+                </div>
+              )
+            })
+          }
 )
 
 export default Step2
