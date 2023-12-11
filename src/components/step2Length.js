85c0a070f1b4432522f95494fd6f9d4d3612f447
Brian Thomas <btdeveloper125@gmail.com>
2023-12-11 05:52:26 -0700

    feat(TermToggle): SET TERM
    added component to trigger term toggler

---
+++ src/components/step2Length.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function Step2Length({tl, termToggle}) {
+  return (
+    <section className="step2-length">
+      <div>Monthly</div>
+      <div className="step2-length__toggle-container">
+        <label className="step2-length__toggle">
+          <input type="checkbox" className="step2-length__input" onClick={() => termToggle()} />
+          <span className="step2-length__slider"></span>
+        </label>
+      </div>
+      <div>Yearly</div>
+    </section>
+  )
+}
+
+export default Step2Length
