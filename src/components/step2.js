3fa949cabb534a5aa283e3ea5b58e975b9d0d607
Brian Thomas <btdeveloper125@gmail.com>
2023-12-13 14:44:34 -0700

    refactor(step2): RESTRUCTURE/CLEANUP
    formatted file
    imported from data file
    updated to use state
    set key value for list
    using imported function for planSelect

--- src/components/step2.js
+++ src/components/step2.js
@@ -1,50 +1,28 @@
 import React, { useEffect, useCallback } from 'react'
 import gsap from 'gsap/gsap-core'
 import Step2Length from './step2Length'
-import Arcade from '../../static/icon-arcade.svg'
-import Advanced from '../../static/icon-advanced.svg'
-import Pro from '../../static/icon-pro.svg'
+import PlanSelect from '../helpers/PlanSelect'
+import planData from '../data/planData'
 import '../styles/step2.css'
-const Cards = [
-  {
-    icon:<Arcade/>,
-    text:'Arcade',
-    price:{
-      monthly: '$9/mo',
-      yearly: '$90/yr'
-    }
-  },
-  {
-    icon:<Advanced/>,
-    text:'Advanced',
-    price:{
-      monthly: '$12/mo',
-      yearly: '$120/yr'
-    }
-  },
-  {
-    icon:<Pro/>,
-    text:'Pro',
-    price:{
-      monthly: '$15/mo',
-      yearly: '$150/yr'
-    }
-  }
-]
-function Step2({planSelect, termToggle}){
-  useEffect(() => {
-    
-  }, [])
-  return(
+
+function Step2({ setPlan, plan }) {
+  useEffect(() => {}, [plan])
+  return (
     <section className="step2">
-    <h1 className="step2-header">Select your plan</h1>
-    <p className="step2-sub">You have the option of monthly or yearly billing.</p>
-    <section className="step2-main">
-      <section className="step2-tier">
+      <h1 className="step2-header">Select your plan</h1>
+      <p className="step2-sub">You have the option of monthly or yearly billing.</p>
+      <section className="step2-main">
+        <section className="step2-tier">
           {
+            planData.map((el) => {
+              return (
+                <div 
+                  key={el.name} 
+                  className={`step2-tier-option ${el.name.toLocaleLowerCase()} ${el.name == plan.name ? "step2-tier-option__active" : ""}`} 
                   onClick={(e) => PlanSelect(e, setPlan, plan)}>
                   <div className="step2-tier-option__inner">
                     {el.icon}
+                    <h1 className="step2-tier-option__text">{el.name}</h1>
                     <p className="step2-tier-option__monthly">{plan.term[0].length}</p>
                     <p className="step2-tier-option__monthly">{plan.term[0].price}</p>
                     <p className="step2-tier-option__free">2 months free</p>
@@ -53,11 +31,11 @@ function Step2({planSelect, termToggle}){
               )
             })
           }
-      </section>
+        </section>
         <Step2Length setPlan={setPlan} plan={plan} />
+      </section>
     </section>
-  </section>  
   )
-        }
+}
 
 export default Step2
