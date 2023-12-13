136205751590a828e02da7ff5b12691a8c77785b
Brian Thomas <btdeveloper125@gmail.com>
2023-12-13 06:54:12 -0700

    refactor(Step4): BASE JSX/CSS

--- src/components/step4.js
+++ src/components/step4.js
@@ -1,10 +1,52 @@
 import * as React from 'react'
-
-const Step4 = () => (
+import '../styles/step4.css'
+const Step4 = ({term}) => (
   <div className="step4">
-    <h1>Finishing up</h1>
-    <p>Double check everything looks OK before confirming.</p>
-    
+    <h1 className="step4-header">Finishing up</h1>
+    <p className="step4-sub">Double check everything looks OK before confirming.</p>
+    <section>
+      <section>
+        <section>
+          <div className="step4-table">
+            <div className="step4-tableBody">
+              <div className="step4-tableRow">
+                <div className="step4-tableCell">
+                  <p className="step4-tableCell__arcade-mo">Arcade (Monthly)</p>
+                  <p className="step4-tableCell__advanced-mo">Advanced (Monthly)</p>
+                  <p className="step4-tableCell__pro-mo">Pro (Monthly)</p>
+                  <p className="step4-tableCell__arcade-ee">Arcade (Yearly)</p>
+                  <p className="step4-tableCell__advanced-ee">Advanced (Yearly)</p>
+                  <p className="step4-tableCell__pro-ee">Pro (Yearly)</p>
+                </div>
+                <div className="step4-tableCell">&nbsp;</div>
+                <div className="step4-tableCell">
+                  <span>$9/</span>
+                  <span>mo</span>
+                </div>
+              </div>
+              <div className="step4-tableRow">
+                <div className="step4-tableCell">
+                  
+                </div>
+                <div className="step4-tableCell">&nbsp;</div>
+                <div className="step4-tableCell">&nbsp;</div>
+              </div>
+              <div className="step4-tableRow">
+                <div className="step4-tableCell">&nbsp;</div>
+                <div className="step4-tableCell">&nbsp;</div>
+                <div className="step4-tableCell">&nbsp;</div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </section>
+      <section>
+      </section>
+      <section>
+      </section>
+      <section>
+      </section>
+    </section>
   </div>
 )
 
