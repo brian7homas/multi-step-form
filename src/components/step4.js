ac106a4aa6d4fc2ff285f949a1f74aed11bc663c
Brian Thomas <btdeveloper125@gmail.com>
2023-12-14 07:31:35 -0700

    feat(step4): SETUP
    added base step 4 component/styles
    rendering plan state successfully

--- src/components/step4.js
+++ src/components/step4.js
@@ -1,53 +1,53 @@
-import * as React from 'react'
+import React, { useEffect } from 'react'
 import '../styles/step4.css'
-const Step4 = () => (
-  <div className="step4">
-    <h1 className="step4-header">Finishing up</h1>
-    <p className="step4-sub">Double check everything looks OK before confirming.</p>
-    <section>
+function Step4({plan}) {
+  useEffect(() =>{},[plan])
+  return(
+    <div className="step4">
+      <h1 className="step4-header">Finishing up</h1>
+      <p className="step4-sub">Double check everything looks OK before confirming.</p>
       <section>
         <section>
-          <div className="step4-table">
-            <div className="step4-tableBody">
-              <div className="step4-tableRow">
-                <div className="step4-tableCell">
-                  <p className="step4-tableCell__arcade-mo">Arcade (Monthly)</p>
-                  <p className="step4-tableCell__advanced-mo">Advanced (Monthly)</p>
-                  <p className="step4-tableCell__pro-mo">Pro (Monthly)</p>
-                  <p className="step4-tableCell__arcade-ee">Arcade (Yearly)</p>
-                  <p className="step4-tableCell__advanced-ee">Advanced (Yearly)</p>
-                  <p className="step4-tableCell__pro-ee">Pro (Yearly)</p>
+          <section>
+            <div className="step4-table">
+              <div className="step4-tableBody">
+                <div className="step4-tableRow">
+                  <div className="step4-tableCell">
+                    <div className="step4-tableCell__plan">
+                      <p className="step4-tableCell__plan-name">{plan.name}</p>
+                      <p className="step4-tableCell__plan-term">({plan.termUser[0].length})</p>
+                    </div>
+                  </div>
+                  <div className="step4-tableCell">&nbsp;</div>
+                  <div className="step4-tableCell">
+                    <p>{plan.termUser[0].price}</p>
+                  </div>
                 </div>
-                <div className="step4-tableCell">&nbsp;</div>
-                <div className="step4-tableCell">
-                  <span>$9/</span>
-                  <span>mo</span>
+                <div className="step4-tableRow">
+                  <div className="step4-tableCell">
+                    
+                  </div>
+                  <div className="step4-tableCell">&nbsp;</div>
+                  <div className="step4-tableCell">&nbsp;</div>
                 </div>
-              </div>
-              <div className="step4-tableRow">
-                <div className="step4-tableCell">
-                  
+                <div className="step4-tableRow">
+                  <div className="step4-tableCell">&nbsp;</div>
+                  <div className="step4-tableCell">&nbsp;</div>
+                  <div className="step4-tableCell">&nbsp;</div>
                 </div>
-                <div className="step4-tableCell">&nbsp;</div>
-                <div className="step4-tableCell">&nbsp;</div>
-              </div>
-              <div className="step4-tableRow">
-                <div className="step4-tableCell">&nbsp;</div>
-                <div className="step4-tableCell">&nbsp;</div>
-                <div className="step4-tableCell">&nbsp;</div>
               </div>
             </div>
-          </div>
+          </section>
+        </section>
+        <section>
+        </section>
+        <section>
+        </section>
+        <section>
         </section>
       </section>
-      <section>
-      </section>
-      <section>
-      </section>
-      <section>
-      </section>
-    </section>
-  </div>
-)
+    </div>
+  )
+}
 
 export default Step4
