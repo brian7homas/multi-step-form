cbfca2647372522c56d7b10c7c0988a8a24112e6
Brian Thomas <btdeveloper125@gmail.com>
2023-12-15 12:32:02 -0700

    refactor(Step2): CARDS
    update classes and els

--- src/components/step2.js
+++ src/components/step2.js
@@ -22,9 +22,8 @@ function Step2({ setPlan, plan, setAddOn }) {
                   <div className="step2-tier-option__inner">
                     {el.icon}
                     <h1 className="step2-tier-option__text">{el.name.charAt(0).toUpperCase() + el.name.slice(1)}</h1>
-                    <p className="step2-tier-option__monthly">{plan.termUser[0].length.charAt(0).toUpperCase() + plan.termUser[0].length.slice(1)}</p>
-                    <p className="step2-tier-option__monthly">{plan.termUser[0].length == 'monthly' ? el.term[0].price : el.term[1].price}</p>
-                    <p className="step2-tier-option__free">2 months free</p>
+                    <p className="step2-tier-option__price">{plan.termUser[0].length == 'monthly' ? el.term[0].price : el.term[1].price}</p>
+                    { plan.termUser[0].length == 'monthly' ? '' : <p className="step2-tier-option__free">2 months free</p>}
                   </div>
                 </div>
               )
